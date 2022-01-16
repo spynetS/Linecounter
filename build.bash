e7f4a954e99407d7f44db59683a9ee7141c9a628
Alfred <alfred@stensatter.se>
2022-01-16 21:07:19 +0100

    made so it saves the build to the environment

--- build.bash
+++ build.bash
@@ -1,12 +1,15 @@
 
 # This is a bash file to help you build the project
-# The file to start when build is done is the Loadlines that will appear in the source folder (not src)
+# The file to start when build is done is the Linecounter that will appear in the source folder (not src)
 # You might have to do chmod 755 ./build.bash to run this script
 
 pip install pyinstaller
 
 pyinstaller --onefile ./src/main.py
 
-cp ./dist/main ./Loadlines
+cp ./dist/main ./Linecounter
+sudo cp ./dist/main /usr/bin/Linecounter
 
-echo "\n Thank you for installing Linecounter"
+export Linecounter=/user/bin/Linecounter 
+
+echo "Thank you for installing Linecounter"
---
+++ build.bat
@@ -0,0 +1,14 @@
+:: This is a bash file to help you build the project
+:: The file to start when build is done is the Linecounter that will appear in the source folder (not src)
+:: You might have to do chmod 755 ./build.bash to run this script
+
+pip install pyinstaller
+
+pyinstaller --onefile ./src/main.py
+
+cp ./dist/main ./Linecounter
+sudo cp ./dist/main /usr/bin/Linecounter
+
+SET Linecounter=";%HOMEDRIVE%\Linecounter"
+
+echo "Thank you for installing Linecounter"
