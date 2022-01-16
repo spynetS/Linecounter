723227f25d6f148a6679cb8576000c73bae5e552
Alfred Roos <alfred@stensatter.se>
2022-01-16 21:43:12 +0100

    build.bat update (environment varable does not work)

--- build.bat
+++ build.bat
@@ -1,14 +1,23 @@
+
 :: This is a bash file to help you build the project
 :: The file to start when build is done is the Linecounter that will appear in the source folder (not src)
 :: You might have to do chmod 755 ./build.bash to run this script
 
+SET mypath=%~dp0
+echo %mypath:~0,-1%
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+CD %mypath:~0,-1%
+
 pip install pyinstaller
 
 pyinstaller --onefile ./src/main.py
+mkdir %HOMEPATH%\Linecounter\
+copy .\dist\main.exe %HOMEPATH%\Linecounter\Linecounter.exe
 
-cp ./dist/main ./Linecounter
-sudo cp ./dist/main /usr/bin/Linecounter
+setx /M newpath %HOMEDRIvE%%HOMEPATH%\Linecounter\
 
-SET Linecounter=";%HOMEDRIVE%\Linecounter"
-
-echo "Thank you for installing Linecounter"
+echo "Installation at %HOMEPATH%\Linecounter\ completed"
+echo "Thank you for installing Linecounter!"
+pause
