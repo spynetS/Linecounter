df33a47909f90090cc0fdf0c46b276f0e2bca169
Alfred <alfred@stensatter.se>
2022-01-19 16:14:05 +0100

    test

--- build.bash
+++ build.bash
@@ -2,6 +2,17 @@
 # This is a bash file to help you build the project
 #This will create the binary and place it in the /usr/bin folder and set the environment variable so you only need to write Linecounter in your terminal
 
+command -v python3 >/dev/null 2>&1 || { 
+    sudo apt-get update
+    sudo apt-get install python3.6
+ }
+
+command -v python >/dev/null 2>&1 || { 
+    sudo apt update
+    sudo apt install python3-pip
+ }
+
+
 
 pip install pyinstaller
 
