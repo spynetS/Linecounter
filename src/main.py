8e0a29399925690dcb9e8bee297f8cb14f6b8036
spy <alfred@stensatter.se>
2023-01-05 22:04:11 +0100

    begun

--- src/main.py
+++ src/main.py
@@ -1,6 +1,31 @@
 
+# list all files and their lines
+# group files and lines
+# total of lines
+# ignore files (defaults)
+# ignore folders (defaults)
+# ignore extetions (defaults)
+# ignore comments
+# ignore empty lines
+from flagser import *
 
+# create flags
+class Lf(Flag):
+    shortFlag = "-lf"
+    description="lists all files and their line counts"
 
+    def onCall(self, args):
+        print("all files and thier line counets")
+
+class T(Flag):
+    shortFlag = "-t"
+    description="outputs total of lines countet"
+
+    def onCall(self, args):
+        print("total")
+# checks flags
+a = FlagManager([Lf(), T()])
+a.check()
 
 
 
