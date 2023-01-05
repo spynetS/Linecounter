ff9c793b0f8d50fdc78cbc52384e60bcdd90ba2d
spy <alfred@stensatter.se>
2023-01-05 21:57:27 +0100

    a new start

--- src/main.py
+++ src/main.py
@@ -1,13 +1,6 @@
-import sys
-import time
 
-from argHandler import ArgHandler
 
-common_extensions = [".py", ".c", ".java", ".cpp", ".sh"]
 
-if(__name__=="__main__"):
-    #start = (time.time())
-    argHandler = ArgHandler(sys.argv)
-    #end = (time.time())
-    #print("Time taken:",end-start)
+
+
 
